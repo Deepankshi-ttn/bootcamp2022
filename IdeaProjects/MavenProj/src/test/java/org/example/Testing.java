0a1cfe9eb4ab9ee359d2b85aa164ae96286c79bd
Deepankshi <deepankshi.arora@tothenew.com>
2022-10-14 16:40:38 +0530

    modified testingFile

--- IdeaProjects/MavenProj/src/test/java/org/example/Testing.java
+++ IdeaProjects/MavenProj/src/test/java/org/example/Testing.java
@@ -1,19 +1,3 @@
-//package org.example;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class Testing {
-//    @Test
-//    public void TestSum()
-//    {
-//       int result = org.example.Test.sum(6,4);
-//        Assert.assertEquals(10,result);
-////        System.out.println("Hello World!");
-//    }
-//
-//}
-//
 package org.example;
 
 import org.junit.Assert;
